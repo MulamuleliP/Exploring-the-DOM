c77310881fdfc5d943bebba2d702f2f3a56c1b06
MulamuleliP <166374862+MulamuleliP@users.noreply.github.com>
2024-07-08 18:16:47 +0200

    initial commit

--- index.js
+++ index.js
@@ -3,32 +3,43 @@ const theMessageButton = document.querySelector('.theMessageButton');
 const clearMessageButton = document.querySelector('.clearMessageButton');
 const inputBox = document.querySelector('.theInputValue');
 const fruitList = document.querySelector('.fruits');
-const addFruitButton = document.querySelector('.addFruitButton');
+const fruitInput = document.querySelector('.inputFruit');
+const addFruitButton = document.querySelector('.addFruitButton')
 
 
 
-theMessageButton.addEventListener('click', function() {
-    setTimeout(function(){
-        myMessage.innerText = inputBox.value
-    }, 3000 ) 
+theMessageButton.addEventListener('click', function () {
+
+    myMessage.innerText = inputBox.value
+
 });
 
-clearMessageButton.addEventListener('click', function() {
+clearMessageButton.addEventListener('click', function () {
     myMessage.innerText = '';
 });
 
-myMessage.addEventListener('click', function() {
+myMessage.addEventListener('click', function () {
     myMessage.classList.toggle('darkmode')
     //myMessage.classList.add('darkmode')
 })
 
 const fruits = ['Apples', 'Pears', 'Oranges', 'Grapes', 'Bananas'];
-for(let i=0;i<fruits.length;i++){
+for (let i = 0; i < fruits.length; i++) {
     // get fruits from the list
     const fruit = fruits[i];
-    
+
     // create a new li element
     const li = document.createElement('li');
     li.innerText = fruit;
     fruitList.appendChild(li);
- }
+}
+
+addFruitButton.addEventListener('click', function () {
+    const li = document.createElement('li');
+    li.innerText = fruitInput.value;
+    fruitList.appendChild(li);
+})
+
+
+
+
