e0f7287169d6492b253fc837f6f536001d92635e
MulamuleliP <166374862+MulamuleliP@users.noreply.github.com>
2024-07-08 21:00:51 +0200

    DOM

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const theMessageButton = document.querySelector('.theMessageButton');
 const clearMessageButton = document.querySelector('.clearMessageButton');
 const inputBox = document.querySelector('.theInputValue');
 const fruitList = document.querySelector('.fruits');
-const fruitInput = document.querySelector('.inputFruit');
-const addFruitButton = document.querySelector('.addFruitButton')
-
-
+const fruitInput = document.querySelector('.InputFruit'); // Corrected case
+const addFruitButton = document.querySelector('.addFruitButton');
 
 theMessageButton.addEventListener('click', function () {
-
-    myMessage.innerText = inputBox.value
-
+    myMessage.innerText = inputBox.value;
 });
 
 clearMessageButton.addEventListener('click', function () {
@@ -19,27 +15,24 @@ clearMessageButton.addEventListener('click', function () {
 });
 
 myMessage.addEventListener('click', function () {
-    myMessage.classList.toggle('darkmode')
-    //myMessage.classList.add('darkmode')
-})
+    myMessage.classList.toggle('darkmode');
+});
 
 const fruits = ['Apples', 'Pears', 'Oranges', 'Grapes', 'Bananas'];
-for (let i = 0; i < fruits.length; i++) {
-    // get fruits from the list
-    const fruit = fruits[i];
 
-    // create a new li element
+for (let i = 0; i < fruits.length; i++) {
+    const fruit = fruits[i];
     const li = document.createElement('li');
     li.innerText = fruit;
     fruitList.appendChild(li);
 }
 
-addFruitButton.addEventListener('click', function () {
-    const li = document.createElement('li');
-    li.innerText = fruitInput.value;
-    fruitList.appendChild(li);
-})
-
-
-
-
+addFruitButton.addEventListener('click', function() {
+    const fruit = fruitInput.value.trim();
+    if (fruit !== '') {
+        const li = document.createElement('li');
+        li.innerText = fruit;
+        fruitList.appendChild(li);
+        fruitInput.value = ''; 
+    }
+});
